95fb65d3dc8f431a4ca3730e000e56e7cdd84443
FABhishek <abhishekasc3@gmail.com>
2022-01-09 17:57:33 +0530

    video-playback added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Home from "./Home.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Room from "./Room.js"
+import "./App.css"
+
 
 function App() {
   return (
--- src/Room.js
+++ src/Room.js
@@ -1,32 +1,66 @@
 import React from "react";
+import { useState } from "react";
 import "./Room.css";
 import "./Home.css";
+import "font-awesome/css/font-awesome.min.css";
+import ReactPlayer from "react-player";
 
 function Room() {
+  const [link, setLink] = useState("");
+  const [playing, setPlaying, PauseIcon] = useState(true)
+
+  const getLink = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="App">
       <nav className="navbar NavColor">
         <a className="textColor" href="/">
           <h1>WatchPawri!!!!</h1>
         </a>
+        <a className="textColor" href="/">
+          <i class="fa-solid fa-wrench-simple"></i>Leave Room
+        </a>
       </nav>
       <div className="content">
         <div className="video-container">
-          <form className="search-form">
+          <form onSubmit={getLink} className="search-form">
             <input
               type="text"
-              name="youtube_url"
-              placeholder="Enter YouTube URL"
-              required
+              value={link}
+              onChange={(e) => setLink(e.target.value)}
+              placeholder="Enter any URL"
             />
             <button type="submit" class="ldbtn">
-              Load Video
+              Load
             </button>
           </form>
-
-          <div id="player"></div>
+          <div id="player">
+            <ReactPlayer url={link}
+             width='100%'
+             height='100%' 
+            //  style={{display:"none"}}
+             controls={false}
+             playing={playing}
+            //  wrapper={"audio"}
+            //  progressInterval={200}
+            //  config={{
+            //    file: {
+            //      attributes: {preload: "auto"},
+            //      forceAudio:true,
+            //    },
+            //  }}
+         />
+         <button size="small">
+             {playing? (
+               <button onClick={() => setPlaying(false)}/>
+             ) : (
+               <button onClick={() => setPlaying(true)}/>
+             )}
+         </button>
+          </div>
         </div>
-
         <div className="sidebar">
           <div className="username-form"></div>
         </div>
