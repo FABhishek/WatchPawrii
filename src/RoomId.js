0cec79dc76e54300c8a426af21108c740be15235
yahya-cloud <yahya08in@gmail.com>
2022-03-06 19:59:12 +0530

    added copy roomId component

---
+++ src/RoomId.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+
+const RoomId = ({ roomId }) => {
+  return (
+    <div className='roomIdContainer'>
+      <h2
+        style={{ cursor: 'pointer' }}
+        onClick={() => {
+          navigator.clipboard.writeText(roomId);
+        }}>
+        {roomId}
+      </h2>
+      <div className='hoverContainer'>
+        <p>Click to copy</p>
+      </div>
+    </div>
+  );
+};
+
+export default RoomId;
